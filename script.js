6dfe85e11a376ef0b498c64f7caf70ab0678b729
Moo <8406421+SpookyMoo@users.noreply.github.com>
2023-10-18 22:10:39 -0500

    1

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ async function convertCurrency() {
     const from_currency = document.getElementById("from_currency").value;
     const to_currency = document.getElementById("to_currency").value;
     
-    // Removed the amount from the endpoint
-    const endpoint = `/api/convert?from_currency=${from_currency}&to_currency=${to_currency}`;
+    // Updated endpoint to match the serverless function
+    const endpoint = `/api/convert?from_currency=${from_currency}&to_currency=${to_currency}&amount=${amount}`;
 
     try {
         const response = await fetch(endpoint);
         const data = await response.json();
 
         if (data.success) {
-            // Multiply the conversion rate by the amount to get the result
-            const convertedAmount = amount * data.rate;
-            document.getElementById("result").innerText = `${amount} ${from_currency} is ${convertedAmount.toFixed(2)} ${to_currency}`;
+            // Use the convertedAmount from the response
+            const convertedAmount = data.convertedAmount;
+            document.getElementById("result").innerText = `${amount} ${from_currency} is ${convertedAmount} ${to_currency}`;
         } else {
             document.getElementById("result").innerText = `Error: ${data.error}`;
         }
