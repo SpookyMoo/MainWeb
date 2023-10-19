248e4ca3bf8a9a28cd5a0e093a24103522ba22e6
Moo <8406421+SpookyMoo@users.noreply.github.com>
2023-10-18 21:13:41 -0500

    1

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-function convertCurrency() {
-    let amount = document.getElementById('amount').value;
-    let from_currency = document.getElementById('from_currency').value;
-    let to_currency = document.getElementById('to_currency').value;
+async function convertCurrency() {
+    const amount = document.getElementById("amount").value;
+    const from_currency = document.getElementById("from_currency").value;
+    const to_currency = document.getElementById("to_currency").value;
+    
+    const endpoint = `/api/convert?from_currency=${from_currency}&to_currency=${to_currency}&amount=${amount}`;
 
-    // Call the serverless function
-    fetch('/api/convert?from_currency=' + from_currency + '&to_currency=' + to_currency + '&amount=' + amount)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                document.getElementById('result').innerText = "Converted Amount: " + data.converted_amount;
-            } else {
-                document.getElementById('result').innerText = "Error: " + data.message;
-            }
-        })
-        .catch(error => {
-            document.getElementById('result').innerText = "Client-side error: " + error;
-        });
+    try {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+
+        if (data.success) {
+            document.getElementById("result").innerText = `${amount} ${from_currency} is ${data.amount} ${to_currency}`;
+        } else {
+            document.getElementById("result").innerText = `Error: ${data.error}`;
+        }
+    } catch (error) {
+        document.getElementById("result").innerText = `Client-side error: ${error.message}`;
+    }
 }
