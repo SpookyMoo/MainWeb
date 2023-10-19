d2e43b064de6532f4f5ce77b5fc8d7066a953ecb
Moo <8406421+SpookyMoo@users.noreply.github.com>
2023-10-18 20:13:34 -0500

    v1

--- Stuff.js
+++ Stuff.js
@@ -1,5 +1,18 @@
-fetch('/api/convert?from_currency=USD&to_currency=BTC')
-    .then(response => response.json())
-    .then(data => {
-        console.log("Conversion rate:", data.rate);
-    });
+function convertCurrency() {
+    // Get values from input fields
+    const amount = document.getElementById('amount').value;
+    const fromCurrency = document.getElementById('from_currency').value.toUpperCase();
+    const toCurrency = document.getElementById('to_currency').value.toUpperCase();
+
+    // Make a request to your serverless function
+    fetch(`/api/convert?from_currency=${fromCurrency}&to_currency=${toCurrency}`)
+        .then(response => response.json())
+        .then(data => {
+            const convertedAmount = amount * data.rate;
+            document.getElementById('result').innerText = `${amount} ${fromCurrency} is approximately ${convertedAmount.toFixed(2)} ${toCurrency}.`;
+        })
+        .catch(error => {
+            console.error("Error fetching conversion rate:", error);
+            document.getElementById('result').innerText = "Error converting currency. Please try again.";
+        });
+}
---
+++ Stuff.js.bak
@@ -0,0 +1,5 @@
+fetch('/api/convert?from_currency=USD&to_currency=BTC')
+    .then(response => response.json())
+    .then(data => {
+        console.log("Conversion rate:", data.rate);
+    });
