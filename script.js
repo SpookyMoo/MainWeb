0481c3df488fd0b873247049e039b44cd1fadb30
Moo <8406421+SpookyMoo@users.noreply.github.com>
2023-10-18 21:08:46 -0500

    1

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 function convertCurrency() {
-    // Get values from input fields
-    const amount = document.getElementById('amount').value;
-    const fromCurrency = document.getElementById('from_currency').value.toUpperCase();
-    const toCurrency = document.getElementById('to_currency').value.toUpperCase();
+    let amount = document.getElementById('amount').value;
+    let from_currency = document.getElementById('from_currency').value;
+    let to_currency = document.getElementById('to_currency').value;
 
-    // Make a request to your serverless function
-   fetch(`/api/convert?from_currency=${fromCurrency}&to_currency=${toCurrency}`)
-        .then(response => {
-            // Log the raw response for debugging
-            console.log("Raw response:", response);
-            return response.text();
-        })
-        .then(text => {
-            // Attempt to parse the text as JSON
-            try {
-                const data = JSON.parse(text);
-                // ... [rest of the conversion logic]
-            } catch (error) {
-                console.error("Error parsing JSON:", error, "Raw text:", text);
+    // Call the serverless function
+    fetch('/api/convert?from_currency=' + from_currency + '&to_currency=' + to_currency + '&amount=' + amount)
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                document.getElementById('result').innerText = "Converted Amount: " + data.converted_amount;
+            } else {
+                document.getElementById('result').innerText = "Error: " + data.message;
             }
         })
         .catch(error => {
-            console.error("Client-side error:", error);
-            document.getElementById('result').innerText = "Client-side error occurred. Please check the console for details.";
+            document.getElementById('result').innerText = "Client-side error: " + error;
         });
 }
