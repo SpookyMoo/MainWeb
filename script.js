1aaa9bdd4d77f5eaceece1c4d516a4b706bfac03
Moo <8406421+SpookyMoo@users.noreply.github.com>
2023-10-23 21:54:44 -0500

    1

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Define a function to perform the currency conversion
 async function convertCurrency() {
     const amount = document.getElementById("amount").value;
-    const Cash = document.getElementById("Cash").value;
-    const toCash = document.getElementById("toCash").value;
+    const fiatFrom = document.getElementById("fiatFrom").value;
+    const cryptoFrom = document.getElementById("cryptoFrom").value;
+    const fiatTo = document.getElementById("fiatTo").value;
+    const cryptoTo = document.getElementById("cryptoTo").value;
 
     // Construct the endpoint URL with the provided values
-    const endpoint = `https://dreadfultech.com/api/convert?Cash=${Cash}&toCash=${toCash}&amount=${amount}`;
+    const endpoint = `https://dreadfultech.com/api/convert?fiatFrom=${fiatFrom}&cryptoFrom=${cryptoFrom}&fiatTo=${fiatTo}&cryptoTo=${cryptoTo}&amount=${amount}`;
 
     try {
         // Make the API request
@@ -14,7 +16,7 @@ async function convertCurrency() {
 
         if (data && data.convertedAmount) {
             // Display the result
-            document.getElementById("result").innerText = `${amount} ${Cash} is approximately ${data.convertedAmount} ${toCash}`;
+            document.getElementById("result").innerText = `${amount} ${fiatFrom || cryptoFrom} is approximately ${data.convertedAmount} ${fiatTo || cryptoTo}`;
         } else {
             document.getElementById("result").innerText = data.error || "An error occurred.";
         }
